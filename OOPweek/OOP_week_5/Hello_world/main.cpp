0711b7c5a4298d4173930bf5eddc642682e0f55b
Tienhuynh9258 <66746075+Tienhuynh9258@users.noreply.github.com>
2020-12-22 11:01:33 +0700

    xoa
    xoa

--- OOPweek/OOP_week_5/Hello_world/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Dummy.h"
-
-int main()
-{
-    cout << "Hello World.\n";
-    return 0;
-}
--- OOPweek/OOP_week_7/Human/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Human.h"
-
-int main()
-{
-    Human H1;
-    H1.setInfo();
-    H1.getInfo();
-    return 0;
-}
